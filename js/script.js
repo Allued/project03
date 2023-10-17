0aaddfc55db5d993d06ab665603676497326375b
Allued <114755125+Allued@users.noreply.github.com>
2023-10-17 15:01:47 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function showitem1(){
+    event.srcElement.style.color = "orange";
+    document.getElementById("1").style.display = "block";
+}
+function leaveitem1(){
+    event.srcElement.style.color = "#757575";
+    document.getElementById("1").style.display = "none";
+}
+
+function showitem2(){
+    event.srcElement.style.color = "orange";
+    document.getElementById("2").style.display = "block";
+}
+function leaveitem2(){
+    event.srcElement.style.color = "#757575";
+    document.getElementById("2").style.display = "none";
+}
+
+function showitem3(){
+    event.srcElement.style.color = "orange";
+    document.getElementById("3").style.display = "block";
+}
+function leaveitem3(){
+    event.srcElement.style.color = "#757575";
+    document.getElementById("3").style.display = "none";
+}
+
+function showitem4(){
+    event.srcElement.style.color = "orange";
+    document.getElementById("4").style.display = "block";
+}
+function leaveitem4(){
+    event.srcElement.style.color = "#757575";
+    document.getElementById("4").style.display = "none";
+}
+
+function showitem5(){
+    event.srcElement.style.color = "orange";
+    document.getElementById("5").style.display = "block";
+}
+function leaveitem5(){
+    event.srcElement.style.color = "#757575";
+    document.getElementById("5").style.display = "none";
+}
